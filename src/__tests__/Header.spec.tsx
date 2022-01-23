695f93befbbd8bcadb0b6024ddec9cafce669be8
matheus oliveira <matheus.skm@hotmail.com>
2022-01-23 01:11:43 -0300

    added a assertion to test if a element with a given alt text is in the document

--- src/__tests__/Header.spec.tsx
+++ src/__tests__/Header.spec.tsx
@@ -4,6 +4,11 @@ import { Header } from "../components";
 describe("Header", () => {
     test("it should render Header component", () => {
         render(<Header/>)
+        expect(screen.getByRole("banner")).toBeInTheDocument()
     });
-    
+    test("it should render an Image element with alt text 'Matheus Oliveira'", () => {
+        render(<Header/>);
+        expect(screen.getByAltText(/matheus\soliveira/i)).toBeInTheDocument();
+    })
+
 });
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,15 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 
 export const Header = () => {
     return (
         <Box 
-            // as="header"
+            as="header"
             w="100%"
             margin="0 auto"
         >
-
+            <Flex>
+                <Image src="/" alt="Matheus Oliveira"/>
+            </Flex>
         </Box>
     )
 }
